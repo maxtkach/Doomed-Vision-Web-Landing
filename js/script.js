cf230810900327de7be2279d1b188f87bd388ad0
Max Tkach <maxtkach4422@gmail.com>
2024-03-07 22:23:46 +0200

    preloader added

--- js/script.js
+++ js/script.js
@@ -329,3 +329,22 @@ function changePositionAndVisibility() {
 }
 // Вызываем функцию первый раз, чтобы начать процесс
 changePositionAndVisibility();
+
+window.onload = function() {
+  setTimeout(function() {
+    var loader = document.querySelector('.preloader-container');
+    loader.classList.add("active_new");
+  //  $('.preloader').addClass('active');
+    // Display your page content after preloader animation completes
+   // document.body.style.overflow = 'auto'; // Restore scrolling
+  }, 8000); // Adjust the duration as needed
+
+  // Apply animations to each span element with a delay
+  const spans = document.querySelectorAll('.preloader-text span');
+  spans.forEach((span, index) => {
+    setTimeout(() => {
+      span.style.opacity = '1';
+      span.style.animation = ' expandWidth 10s forwards, fadeOut 5s forwards'; // Add fade-out animation
+    }, index * 300); // Adjust the delay between each letter's animation
+  });
+};
